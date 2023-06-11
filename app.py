983d218576b2bac97b1529506175f6af4c73f266
Eduardo Motta de Moraes <edumoraes@live.com>
2023-06-10 23:33:53 -0300

    changes for deployment

--- app.py
+++ app.py
@@ -12,52 +12,49 @@ from service import (
     get_countries_rel,
 )
 
-server = None
+
+app = Dash(__name__)
+server = app.server
+
+app.layout = html.Div(
+    [
+        html.H4("Country populations by year"),
+        dcc.Dropdown(
+            id="dropdown",
+            options=get_countries_rel(),
+            value=["NLD"],
+            multi=True,
+        ),
+        dcc.Graph(id="graph"),
+    ]
+)
 
 
-def main() -> None:
-    app = Dash(__name__)
-    server = app.server
+async def update_graph(countries_list: List[str]):
+    if len(countries_list) > 0:
+        countries = await get_countries(countries_list)
+        countries_df = await get_countries_as_df(countries_list)
+        populations_df = get_populations_as_df([c.id for c in countries])
 
-    app.layout = html.Div(
-        [
-            html.H4("Country populations by year"),
-            dcc.Dropdown(
-                id="dropdown",
-                options=get_countries_rel(),
-                value=["NLD"],
-                multi=True,
-            ),
-            dcc.Graph(id="graph"),
-        ]
-    )
+        plot_df = join_population_and_countries_for_plotting_df(
+            countries_df, populations_df
+        )
 
-    async def update_graph(countries_list: List[str]):
-        if len(countries_list) > 0:
-            countries = await get_countries(countries_list)
-            countries_df = await get_countries_as_df(countries_list)
-            populations_df = get_populations_as_df([c.id for c in countries])
+    else:
+        plot_df = pd.DataFrame({"year": [], "population": [], "Country": []})
 
-            plot_df = join_population_and_countries_for_plotting_df(
-                countries_df, populations_df
-            )
+    fig = px.line(plot_df, x="year", y="population", color="Country")
+    fig.update_layout(xaxis_title="Year", yaxis_title="Population")
+    return fig
 
-        else:
-            plot_df = pd.DataFrame({"year": [], "population": [], "Country": []})
 
-        fig = px.line(plot_df, x="year", y="population", color="Country")
-        fig.update_layout(xaxis_title="Year", yaxis_title="Population")
-        return fig
-
-    @app.callback(Output("graph", "figure"), Input("dropdown", "value"))
-    def initiate_async_update(countries_list: List[str]):
-        loop = asyncio.new_event_loop()
-        result = loop.run_until_complete(update_graph(countries_list))
-        loop.close()
-        return result
-
-    app.run()
+@app.callback(Output("graph", "figure"), Input("dropdown", "value"))
+def initiate_async_update(countries_list: List[str]):
+    loop = asyncio.new_event_loop()
+    result = loop.run_until_complete(update_graph(countries_list))
+    loop.close()
+    return result
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run()
